1ff303a8ef9d7d7baa5fe85dcf2d31083e12620c
yusseef <yussef.raouf11@gmail.com>
2021-07-09 16:15:14 +0200

    add products

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
 import {GlobalStyle} from './GlobalStyles'
 import HeroSection from './components/Herosection'
+import Products from './components/Products'
+import { ProductData } from './components/Products/data'
 function App() {
   return (
     <Router>
       <GlobalStyle />
       
       <HeroSection />
+      <Products heading="Choose Your meal" data={ProductData}/>
     </Router>
   );
 }
---
+++ src/components/Products/data.js
@@ -0,0 +1,30 @@
+import product1 from '../../images/product-1.jpg'
+import product2 from '../../images/product-2.jpg'
+import product3 from '../../images/product-3.jpg'
+export const ProductData = [
+    
+    {
+    img: product1,
+    alt:'Chicken burger',
+    name:'Chicken Burger',
+    desc: 'Bread, One peace of fried chicken, Onion, Tomato, Mayonnaise, Hot sauce, Turkey',
+    price :'$30',
+    button: 'Add Chicken burger to cart '
+},
+{
+    img: product2,
+    alt:'Snack box',
+    name:'Snack box',
+    desc: 'Bread, Two piece of fried chickens, coleslaw, French Fries',
+    price :'$15',
+    button: 'Add Snack box to cart'
+},
+{
+    img: product3,
+    alt:'Zinger',
+    name:'Zinger',
+    desc: 'Bread, One large piece of chicken, lettuce, turkey',
+    price :'$20',
+    button: 'Add Zinger to cart'
+},
+]
